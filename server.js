fbeaf75b28311b2380160e01360df1b98a8e1c47
Praful Rana <prafulrana@gmail.com>
2014-10-15 12:51:10 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,48 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello 5\n');
-}).listen(80);
-console.log('Server running at http://127.0.0.1:80/');
+var express = require('express');
+
+// Mongoose import
+var mongoose = require('mongoose');
+
+// Mongoose connection to MongoDB (ted/ted is readonly)
+mongoose.connect('mongodb://ted:ted@ds061797.mongolab.com:61797/theenlighteneddeveloper', function (error) {
+    if (error) {
+        console.log(error);
+    }
+});
+
+// Mongoose Schema definition
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+    first_name: String,
+    last_name: String,
+    email: String
+});
+
+// Mongoose Model definition
+var User = mongoose.model('users', UserSchema);
+
+// Bootstrap express
+var app = express();
+
+// URLS management
+
+app.get('/', function (req, res) {
+    res.send("<a href='/users'>Show Users</a>");
+});
+
+app.get('/users', function (req, res) {
+    User.find({}, function (err, docs) {
+        res.json(docs);
+    });
+});
+
+app.get('/users/:email', function (req, res) {
+    if (req.params.email) {
+        User.find({ email: req.params.email }, function (err, docs) {
+            res.json(docs);
+        });
+    }
+});
+
+// Start the server
+app.listen(80);
