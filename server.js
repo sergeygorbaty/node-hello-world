7a7a4152ed7fa107949ada1829a022319583cf29
sergeygorbaty <sergey.gorbaty@salesforce.com>
2015-10-04 16:39:22 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello World ! /no props');
+    res.send('Hello World!/no more props');
 });
 
 app.listen(80);
